d44a58b2f2af49f978fec4dd2c77c20404837169
Jayanth <jayanthacharya4@gmail.com>
2021-01-12 00:43:28 +0530

    Added fetch

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -4,54 +4,27 @@ import './home.scss'
 
 const Home = () => {
 
-  const [blogs, updateBlogs] = useState([
-    {
-      title: 'How I trained a dragon',
-      author: 'Hiccup Horrendous',
-      id: 1,
-      content: 'Lorem ipsum',
-    },
-    {
-      title: 'How to escape Jumanji',
-      author: 'Robin Williams',
-      id: 2,
-      content: 'Lorem ipsum',
-    },
-    {
-      title: 'Where is the hidden temple',
-      author: 'Indiana Jones',
-      id: 3,
-      content: 'Lorem ipsum',
-    },
-    {
-      title: 'How Micah Bell was the rat',
-      author: 'Arthur Morgan',
-      id: 4,
-      content: 'Lorem ipsum',
-    },
-    {
-      title: 'On the road to valentine',
-      author: 'Arthur Morgan',
-      id: 5,
-      content: 'Lorem ipsum',
-    }
-  ])
+  const [blogs, updateBlogs] = useState<any[]>([])
 
   const handleDelete = (id : number) => {
-    let filterBlogs = blogs.filter(blog => blog.id !== id)
-    updateBlogs(filterBlogs)
+    if(blogs.length) {
+      let filterBlogs = blogs.filter(blog => blog?.id !== id)
+      updateBlogs(filterBlogs)
+    }
   }
 
-
-  const [name, setstate] = useState('mario')
-
+  const [name, setstate] = useState<string>('mario')
   useEffect(() => {
-    console.log("NAME changeed", name)
-  },[name])
+    fetch('http://localhost:8000/blogs').then(res => {
+      return res.json()
+    }).then(data => {
+      updateBlogs(data)
+    })
+  },[])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
       {name}
       <button onClick={() => setstate( Math.random().toString() + 'lugi')}>CHANge</button>
     </div>
