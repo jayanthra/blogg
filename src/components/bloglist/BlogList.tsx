26a95abfbc51e6851222ca39e1ccd7d2bd270501
Jayanth <jayanthacharya4@gmail.com>
2021-01-12 00:57:04 +0530

    Added progress and error

--- src/components/bloglist/BlogList.tsx
+++ src/components/bloglist/BlogList.tsx
@@ -3,7 +3,7 @@ const BlogList = (props: any) => {
   const { blogs, title } = props
   return (
     <div className="blog-list">
-      <h2>{title}</h2>
+      <h2>{blogs.length ? title : ''}</h2>
       {blogs.map((blog: any) => (
         <div className="blog" key={blog.id}>
           <h3>{blog.title}</h3>
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,17 +5,29 @@ import './home.scss'
 const Home = () => {
 
   const [blogs, updateBlogs] = useState<any[]>([])
+  const [isPending, setisPending] = useState<boolean>(true)
+  const [errorMessage, setErrorMessage] = useState<string>('')
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs').then(res => {
+      if(!res.ok) {
+        throw Error('Resourse not found')
+      }
       return res.json()
     }).then(data => {
       updateBlogs(data)
+      setisPending(false)
+      setErrorMessage('')
+    }).catch(err => {
+      setErrorMessage(err.message)
+      setisPending(false)
     })
   },[])
 
   return (
     <div className="home">
+      {errorMessage && <p>{errorMessage}</p>}
+      {isPending && <div>loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   );
