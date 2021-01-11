c983e0f15fae3e5c96d67897003274f95e282983
Jayanth <jayanthacharya4@gmail.com>
2021-01-12 00:44:11 +0530

    cleanup

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -6,14 +6,6 @@ const Home = () => {
 
   const [blogs, updateBlogs] = useState<any[]>([])
 
-  const handleDelete = (id : number) => {
-    if(blogs.length) {
-      let filterBlogs = blogs.filter(blog => blog?.id !== id)
-      updateBlogs(filterBlogs)
-    }
-  }
-
-  const [name, setstate] = useState<string>('mario')
   useEffect(() => {
     fetch('http://localhost:8000/blogs').then(res => {
       return res.json()
@@ -24,9 +16,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
-      {name}
-      <button onClick={() => setstate( Math.random().toString() + 'lugi')}>CHANge</button>
+      {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   );
 }
